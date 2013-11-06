ba086fd9584bc71d5b70dfe87a52aab696a5abde
John Keiser <jkeiser@opscode.com>
2013-11-06 09:19:55 -0800

    Add patchy attribute and cookbook setters

--- providers/environment.rb
+++ providers/environment.rb
@@ -4,14 +4,15 @@ end
 
 action :create do
   # TODO nice json diff of field internals for attrs and run list and such
-  different_fields = new_json.keys.select { |key| new_json[key] != current_json[key] }.to_a
+  desired_json = new_environment_json
+  different_fields = desired_json.keys.select { |key| desired_json[key] != current_json[key] }.to_a
 
   if current_resource_exists?
     if different_fields.size > 0
       description = [ "update environment #{new_resource.name} at #{rest.url}" ]
       description += different_fields.map { |field| "change #{field} from #{current_json[field].inspect} to #{new_json[field].inspect}" }
       converge_by description do
-        rest.put("environments/#{new_resource.name}", normalize_for_put(new_json))
+        rest.put("environments/#{new_resource.name}", normalize_for_put(desired_json))
         Chef::Log.info("#{new_resource} updated environment #{new_resource.name} at #{rest.url}")
       end
     end
@@ -19,7 +20,7 @@ action :create do
     description = [ "create environment #{new_resource.name} at #{rest.url}" ]
     description += different_fields.map { |field| "set #{field} to #{new_json[field].inspect}"}
     converge_by description do
-      rest.post("environments", normalize_for_post(new_json))
+      rest.post("environments", normalize_for_post(desired_json))
       Chef::Log.info("#{new_resource} created environment #{new_resource.name} at #{rest.url}")
     end
   end
@@ -46,6 +47,48 @@ def load_current_resource
   end
 end
 
+def new_environment_json
+  result = new_json
+  # Apply modifiers
+  result['default_attributes'] = apply_modifiers(new_resource.default_attribute_modifiers, result['default_attributes'])
+  result['override_attributes'] = apply_modifiers(new_resource.override_attribute_modifiers, result['override_attributes'])
+  result
+end
+
+def apply_modifiers(modifiers, json)
+  return json if !modifiers || modifiers.size == 0
+
+  # If the attributes have nothing, set them to {} so we have something to add to
+  if json
+    json = Marshal.load(Marshal.dump(json)) # Deep copy
+  else
+    json = {}
+  end
+
+  modifiers.each do |path, value|
+    path = [path] if !path.kind_of?(Array)
+    parent = path[0..-2].inject(json) { |hash, path_part| hash ? hash[path_part] : nil }
+    existing_value = parent ? parent[path[-1]] : nil
+
+    if value.is_a?(Proc)
+      value = value.call(existing_value)
+    end
+    if value == :delete
+      parent.delete(path[-1]) if parent
+      # TODO clean up parent chain if hash is completely emptied
+    else
+      if !parent
+        # Create parent if necessary
+        parent = path[0..-2].inject(json) do |hash, path_part|
+          hash[path_part] = {} if !hash[path_part]
+          hash[path_part]
+        end
+      end
+      parent[path[-1]] = value
+    end
+  end
+  json
+end
 
 #
 # Helpers
--- resources/environment.rb
+++ resources/environment.rb
@@ -11,13 +11,44 @@ attribute :cookbook_versions, :kind_of => Hash, :callbacks => {
 attribute :default_attributes, :kind_of => Hash
 attribute :override_attributes, :kind_of => Hash
 
-# TODO Patchy methods: setting these will patch up attributes without deleting things (unless complete is specified)
-# cookbook_version 'apache', '= 1.0.0'
-# cookbook_version 'apache', :delete
-# default_attribute 'ip_address', '127.0.0.1'
-# default_attribute 'ip_address', :delete
-# override_attribute 'ip_address', '127.0.0.1'
-# override_attribute [ 'pushy', 'port' ], '8000'
-
-# Control options
+# Specifies that this is a complete specification for the environment (i.e. attributes you don't specify will be
+# reset to their defaults)
 attribute :complete, :kind_of => [TrueClass, FalseClass]
+
+NOT_PASSED=Object.new
+
+# default_attribute 'ip_address', '127.0.0.1'
+# default_attribute [ 'pushy', 'port' ], '9000'
+# default_attribute 'ip_addresses' do |existing_value|
+#   (existing_value || []) + [ '127.0.0.1' ]
+# end
+# default_attribute 'ip_address', :delete
+attr_reader :default_attribute_modifiers
+def default_attribute(attribute_path, value=NOT_PASSED, &block)
+  @default_attribute_modifiers ||= []
+  if value != NOT_PASSED
+    @default_attribute_modifiers << [ attribute_path, value ]
+  elsif block
+    @default_attribute_modifiers << [ attribute_path, block ]
+  else
+    raise "default_attribute requires either a value or a block"
+  end
+end
+
+# override_attribute 'ip_address', '127.0.0.1'
+# override_attribute [ 'pushy', 'port' ], '9000'
+# override_attribute 'ip_addresses' do |existing_value|
+#   (existing_value || []) + [ '127.0.0.1' ]
+# end
+# override_attribute 'ip_address', :delete
+attr_reader :override_attribute_modifiers
+def override_attribute(attribute_path, value=NOT_PASSED, &block)
+  @override_attribute_modifiers ||= []
+  if value != NOT_PASSED
+    @override_attribute_modifiers << [ attribute_path, value ]
+  elsif block
+    @override_attribute_modifiers << [ attribute_path, block ]
+  else
+    raise "override_attribute requires either a value or a block"
+  end
+end
