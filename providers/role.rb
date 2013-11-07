f4a8694389c122f36c3950d82b45073837288da1
John Keiser <jkeiser@opscode.com>
2013-11-06 16:42:19 -0800

    Add role resource

--- providers/role.rb
+++ providers/role.rb
@@ -8,35 +8,35 @@ action :create do
 
   if current_resource_exists?
     if different_fields.size > 0
-      description = [ "update environment #{new_resource.name} at #{rest.url}" ]
+      description = [ "update role #{new_resource.name} at #{rest.url}" ]
       description += different_fields.map { |field| "change #{field} from #{current_json[field].inspect} to #{new_json[field].inspect}" }
       converge_by description do
-        rest.put("environments/#{new_resource.name}", normalize_for_put(new_json))
-        Chef::Log.info("#{new_resource} updated environment #{new_resource.name} at #{rest.url}")
+        rest.put("roles/#{new_resource.name}", normalize_for_put(new_json))
+        Chef::Log.info("#{new_resource} updated role #{new_resource.name} at #{rest.url}")
       end
     end
   else
-    description = [ "create environment #{new_resource.name} at #{rest.url}" ]
+    description = [ "create role #{new_resource.name} at #{rest.url}" ]
     description += different_fields.map { |field| "set #{field} to #{new_json[field].inspect}"}
     converge_by description do
-      rest.post("environments", normalize_for_post(new_json))
-      Chef::Log.info("#{new_resource} created environment #{new_resource.name} at #{rest.url}")
+      rest.post("roles", normalize_for_post(new_json))
+      Chef::Log.info("#{new_resource} created role #{new_resource.name} at #{rest.url}")
     end
   end
 end
 
 action :delete do
   if current_resource_exists?
-    converge_by "delete environment #{new_resource.name} at #{rest.url}" do
-      rest.delete("environments/#{new_resource.name}")
-      Chef::Log.info("#{new_resource} deleted environment #{new_resource.name} at #{rest.url}")
+    converge_by "delete role #{new_resource.name} at #{rest.url}" do
+      rest.delete("roles/#{new_resource.name}")
+      Chef::Log.info("#{new_resource} deleted role #{new_resource.name} at #{rest.url}")
     end
   end
 end
 
 def load_current_resource
   begin
-    @current_resource = json_to_resource(rest.get("environments/#{new_resource.name}"))
+    @current_resource = json_to_resource(rest.get("roles/#{new_resource.name}"))
   rescue Net::HTTPServerException => e
     if e.response.code == "404"
       @current_resource = not_found_resource
@@ -48,6 +48,7 @@ end
 
 def new_json_override(json)
   # Apply modifiers
+  json['run_list'] = apply_run_list_modifiers(new_resource.run_list_modifiers, new_resource.run_list_removers, json['run_list'])
   json['default_attributes'] = apply_modifiers(new_resource.default_attribute_modifiers, json['default_attributes'])
   json['override_attributes'] = apply_modifiers(new_resource.override_attribute_modifiers, json['override_attributes'])
   json
@@ -57,18 +58,18 @@ end
 # Helpers
 #
 # Gives us new_json, current_json, not_found_json, etc.
-require 'chef/chef_fs/data_handler/environment_data_handler'
+require 'chef/chef_fs/data_handler/role_data_handler'
 
 include Cheffish::ProviderHelpers
 
 def resource_class
-  Chef::Resource::CheffishEnvironment
+  Chef::Resource::CheffishRole
 end
 
 def data_handler
-  Chef::ChefFS::DataHandler::EnvironmentDataHandler.new
+  Chef::ChefFS::DataHandler::RoleDataHandler.new
 end
 
 def keys
-  %w(name description cookbook_versions default_attributes override_attributes)
+  %w(name description run_list env_run_lists default_attributes override_attributes)
 end
