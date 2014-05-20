c0d8ecdc48d37366bbb997649a4a8e30911e413b
John Keiser <jkeiser@opscode.com>
2014-05-20 07:48:44 -0700

    Add ability to merge arrays

--- lib/cheffish/merged_config.rb
+++ lib/cheffish/merged_config.rb
@@ -2,30 +2,40 @@ module Cheffish
   class MergedConfig
     def initialize(*configs)
       @configs = configs
+      @merge_arrays = {}
     end
 
     attr_reader :configs
+    def merge_arrays(*symbols)
+      symbols.each do |symbol|
+        @merge_arrays[symbol] = true
+      end
+    end
 
     def [](name)
-      result_configs = []
-      configs.each do |config|
-        value = config[name]
-        if !value.nil?
-          if value.respond_to?(:keys)
-            result_configs << value
-          elsif result_configs.size > 0
-            return result_configs[0]
-          else
-            return value
+      if @merge_arrays[name]
+        configs.select { |c| !c[name].nil? }.collect_concat { |c| c[name] }
+      else
+        result_configs = []
+        configs.each do |config|
+          value = config[name]
+          if !value.nil?
+            if value.respond_to?(:keys)
+              result_configs << value
+            elsif result_configs.size > 0
+              return result_configs[0]
+            else
+              return value
+            end
           end
         end
-      end
-      if result_configs.size > 1
-        MergedConfig.new(*result_configs)
-      elsif result_configs.size == 1
-        result_configs[0]
-      else
-        nil
+        if result_configs.size > 1
+          MergedConfig.new(*result_configs)
+        elsif result_configs.size == 1
+          result_configs[0]
+        else
+          nil
+        end
       end
     end
 
