6d6cafebc0dc89f5770fa9e3aa6e3531cba762f8
John Keiser <jkeiser@opscode.com>
2014-08-26 16:48:17 -0700

    Add members and invites to chef_organization

--- lib/chef/provider/chef_organization.rb
+++ lib/chef/provider/chef_organization.rb
@@ -24,6 +24,76 @@ class Chef::Provider::ChefOrganization < Cheffish::ChefProviderBase
         rest.post("/organizations", normalize_for_post(new_json))
       end
     end
+
+    # Revoke invites and memberships when asked
+    invites_to_remove.each do |user|
+      if outstanding_invites.has_key?(user)
+        converge_by "revoke #{user}'s invitation to organization #{new_resource.name}" do
+          rest.delete("/organizations/#{new_resource.name}/association_requests/#{outstanding_invites[user]}")
+        end
+      end
+    end
+    members_to_remove.each do |user|
+      if existing_members.include?(user)
+        converge_by "remove #{user} from organization #{new_resource.name}" do
+          rest.delete("/organizations/#{new_resource.name}/users/#{user}")
+        end
+      end
+    end
+
+    # Invite and add members when asked
+    new_resource.invites.each do |user|
+      if !existing_members.include?(user) && !outstanding_invites.has_key?(user)
+        converge_by "invite #{user} to organization #{new_resource.name}" do
+          rest.post("/organizations/#{new_resource.name}/association_requests", { 'user' => user })
+        end
+      end
+    end
+    new_resource.members.each do |user|
+      if !existing_members.include?(user)
+        converge_by "Add #{user} to organization #{new_resource.name}" do
+          rest.post("/organizations/#{new_resource.name}/users/#{user}", {})
+        end
+      end
+    end
+  end
+
+  def existing_members
+    @existing_members ||= rest.get("/organizations/#{new_resource.name}/users").map { |u| u['user']['username'] }
+  end
+
+  def outstanding_invites
+    @outstanding_invites ||= begin
+      invites = {}
+      rest.get("/organizations/#{new_resource.name}/association_requests").each do |r|
+        invites[r['username']] = r['id']
+      end
+      invites
+    end
+  end
+
+  def invites_to_remove
+    if new_resource.complete
+      if new_resource.invites_specified? || new_resource.members_specified?
+        (existing_members | outstanding_invites.keys) - (new_resource.invites | new_resource.members)
+      else
+        []
+      end
+    else
+      new_resource.remove_members
+    end
+  end
+
+  def members_to_remove
+    if new_resource.complete
+      if new_resource.members_specified?
+        (existing_members | outstanding_invites.keys) - (new_resource.invites | new_resource.members)
+      else
+        []
+      end
+    else
+      new_resource.remove_members
+    end
   end
 
   action :delete do
--- spec/integration/chef_organization_spec.rb
+++ spec/integration/chef_organization_spec.rb
@@ -2,11 +2,14 @@ require 'support/spec_support'
 require 'chef/resource/chef_organization'
 require 'chef/provider/chef_organization'
 
-describe Chef::Resource::ChefOrganization do
+describe Chef::Resource::ChefOrganization, :focus do
   extend SpecSupport
 
   when_the_chef_server 'is empty', :osc_compat => false, :single_org => false do
-    it 'Converging chef_organization "x" creates the organization' do
+    user 'u', {}
+    user 'u2', {}
+
+    it 'chef_organization "x" creates the organization' do
       run_recipe do
         chef_organization 'x'
       end
@@ -14,7 +17,7 @@ describe Chef::Resource::ChefOrganization do
       expect(get('/organizations/x')['full_name']).to eq('x')
     end
 
-    it 'Converging chef_organization "x" with full_name creates the organization' do
+    it 'chef_organization "x" with full_name creates the organization' do
       run_recipe do
         chef_organization 'x' do
           full_name 'Hi'
@@ -23,12 +26,42 @@ describe Chef::Resource::ChefOrganization do
       expect(chef_run).to have_updated('chef_organization[x]', :create)
       expect(get('/organizations/x')['full_name']).to eq('Hi')
     end
+
+    it 'chef_organization "x" and inviting users creates the invites' do
+      run_recipe do
+        chef_organization 'x' do
+          invites 'u', 'u2'
+        end
+      end
+      expect(chef_run).to have_updated('chef_organization[x]', :create)
+      expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(u u2))
+    end
+
+    it 'chef_organization "x" adds members' do
+      run_recipe do
+        chef_organization 'x' do
+          members 'u', 'u2'
+        end
+      end
+      expect(chef_run).to have_updated('chef_organization[x]', :create)
+      expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(u u2))
+    end
   end
 
   when_the_chef_server 'has an organization named x', :osc_compat => false, :single_org => false do
-    organization 'x', { 'full_name' => 'Lo' }
+    user 'u', {}
+    user 'u2', {}
+    user 'u3', {}
+    user 'member', {}
+    user 'member2', {}
+    user 'invited', {}
+    user 'invited2', {}
+    organization 'x', { 'full_name' => 'Lo' } do
+      org_member 'member', 'member2'
+      org_invite 'invited', 'invited2'
+    end
 
-    it 'Converging chef_organization "x" changes nothing' do
+    it 'chef_organization "x" changes nothing' do
       run_recipe do
         chef_organization 'x'
       end
@@ -36,7 +69,7 @@ describe Chef::Resource::ChefOrganization do
       expect(get('/organizations/x')['full_name']).to eq('Lo')
     end
 
-    it 'Converging chef_organization "x" with "complete true" reverts the full_name' do
+    it 'chef_organization "x" with "complete true" reverts the full_name' do
       run_recipe do
         chef_organization 'x' do
           complete true
@@ -46,7 +79,7 @@ describe Chef::Resource::ChefOrganization do
       expect(get('/organizations/x')['full_name']).to eq('x')
     end
 
-    it 'Converging chef_organization "x" with new full_name updates the organization' do
+    it 'chef_organization "x" with new full_name updates the organization' do
       run_recipe do
         chef_organization 'x' do
           full_name 'Hi'
@@ -54,6 +87,135 @@ describe Chef::Resource::ChefOrganization do
       end
       expect(chef_run).to have_updated('chef_organization[x]', :create)
       expect(get('/organizations/x')['full_name']).to eq('Hi')
+    end
+
+    context 'invites and membership tests' do
+      it 'chef_organization "x" and inviting users creates the invites' do
+        run_recipe do
+          chef_organization 'x' do
+            invites 'u', 'u2'
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited invited2 u u2))
+      end
+
+      it 'chef_organization "x" adds members' do
+        run_recipe do
+          chef_organization 'x' do
+            members 'u', 'u2'
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2 u u2))
+      end
+
+      it 'chef_organization "x" does nothing when inviting already-invited users and members' do
+        run_recipe do
+          chef_organization 'x' do
+            invites 'invited', 'member'
+          end
+        end
+        expect(chef_run).not_to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited invited2))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2))
+      end
+
+      it 'chef_organization "x" does nothing when adding members who are already members' do
+        run_recipe do
+          chef_organization 'x' do
+            members 'member'
+          end
+        end
+        expect(chef_run).not_to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited invited2))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2))
+      end
+
+      it 'chef_organization "x" upgrades invites to members when asked' do
+        run_recipe do
+          chef_organization 'x' do
+            members 'invited'
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(invited member member2))
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited2))
+      end
+
+      it 'chef_organization "x" removes members and invites when asked' do
+        run_recipe do
+          chef_organization 'x' do
+            remove_members 'invited', 'member'
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited2))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member2))
+      end
+
+      it 'chef_organization "x" does nothing when asked to remove non-members' do
+        run_recipe do
+          chef_organization 'x' do
+            remove_members 'u', 'u2'
+          end
+        end
+        expect(chef_run).not_to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited invited2))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2))
+      end
+
+      it 'chef_organization "x" with "complete true" reverts the full_name but does not remove invites or members' do
+        run_recipe do
+          chef_organization 'x' do
+            complete true
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x')['full_name']).to eq('x')
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited invited2))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2))
+      end
+
+      it 'chef_organization "x" with members [] and "complete true" removes invites and members' do
+        run_recipe do
+          chef_organization 'x' do
+            members []
+            complete true
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x')['full_name']).to eq('x')
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq([])
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq([])
+      end
+
+      it 'chef_organization "x" with members [] and "complete true" removes invites but not members' do
+        run_recipe do
+          chef_organization 'x' do
+            invites []
+            complete true
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x')['full_name']).to eq('x')
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq([])
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member member2))
+      end
+
+      it 'chef_organization "x" with invites, members and "complete true" removes all non-specified invites and members' do
+        run_recipe do
+          chef_organization 'x' do
+            invites 'invited', 'u'
+            members 'member', 'u2'
+            complete true
+          end
+        end
+        expect(chef_run).to have_updated('chef_organization[x]', :create)
+        expect(get('/organizations/x')['full_name']).to eq('x')
+        expect(get('/organizations/x/association_requests').map { |u| u['username'] }).to eq(%w(invited u))
+        expect(get('/organizations/x/users').map { |u| u['user']['username'] }).to eq(%w(member u2))
+      end
     end
   end
 end
