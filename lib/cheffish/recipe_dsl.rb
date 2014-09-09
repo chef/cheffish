e3c7a328ca285f3e572ce89d91a6bb5e6aa0940a
John Ewart <john@unixninjas.org>
2014-09-09 16:49:54 -0700

    If the user has specified options in with_chef_local_server or other setting as symbols then honor those. Fixes #163

--- lib/cheffish/recipe_dsl.rb
+++ lib/cheffish/recipe_dsl.rb
@@ -72,14 +72,24 @@ class Chef
 
           # Ensure all paths are given
           %w(acl client cookbook container data_bag environment group node role).each do |type|
-            options["#{type}_path"] ||= begin
-              if options[:chef_repo_path].kind_of?(String)
+            # Set the options as string keys (for chef_fs for now)
+            # This is just silly.
+            string_key = "#{type}_path"
+            symbol_key = "#{type}_path".to_sym
+
+            options[string_key] ||= begin
+              if options[symbol_key]
+                options[symbol_key]
+              elsif options[:chef_repo_path].kind_of?(String)
                 Chef::Util::PathHelper.join(options[:chef_repo_path], "#{type}s")
               else
                 options[:chef_repo_path].map { |path| Chef::Util::PathHelper.join(path, "#{type}s")}
               end
             end
-          end
+
+            # Copy over to symbol key for things that use symbol keys...
+            options[symbol_key] = options[string_key]
+          end 
 
           chef_fs = Chef::ChefFS::Config.new(options).local_fs
           chef_fs.write_pretty_json = true
