8ab9066cc6e728587f9b21ddca4c8b2741df6c65
John Keiser <john@johnkeiser.com>
2015-03-19 21:29:02 -0700

    Pass hash options through to with_chef_server correctly

--- lib/cheffish/rspec/chef_run_support.rb
+++ lib/cheffish/rspec/chef_run_support.rb
@@ -9,9 +9,9 @@ module Cheffish
     module ChefRunSupport
       include ChefZero::RSpec
 
-      def when_the_chef_12_server(*args, &block)
+      def when_the_chef_12_server(*args, **options, &block)
         if Gem::Version.new(ChefZero::VERSION) >= Gem::Version.new('3.1')
-          when_the_chef_server(*args, :osc_compat => false, :single_org => false, &block)
+          when_the_chef_server(*args, :osc_compat => false, :single_org => false, **options, &block)
         end
       end
 
