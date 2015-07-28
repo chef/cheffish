d8601e14493fe40468e6bdbce20bfd047de8cad5
John Keiser <john@johnkeiser.com>
2015-07-28 16:00:58 -0600

    Call methods on the example (such as let) if no recipe method is found

--- lib/cheffish/rspec/recipe_run_wrapper.rb
+++ lib/cheffish/rspec/recipe_run_wrapper.rb
@@ -13,7 +13,11 @@ module Cheffish
       def client
         if !@client
           super
+          example = recipe.binding.eval('self')
           @client.load_block(&recipe)
+          @client.define_singleton_method(:method_missing) do |name, *args, &block|
+            example.public_send(name, *args, &block)
+          end
         end
         @client
       end
