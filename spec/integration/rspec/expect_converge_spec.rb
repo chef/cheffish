a0b632b549f1469fd12d1bc4e95504721fb841f8
John Keiser <john@johnkeiser.com>
2015-07-28 20:08:44 -0600

    Make sure resources inside the recipe can access let variables

--- spec/integration/rspec/expect_converge_spec.rb
+++ spec/integration/rspec/expect_converge_spec.rb
@@ -4,14 +4,34 @@ require 'cheffish/rspec/chef_run_support'
 describe 'Cheffish::RSpec::ChefRunSupport#expect_converge' do
   extend Cheffish::RSpec::ChefRunSupport
 
-  context "when there is a let variable" do
-    let(:let_variable) { 'hello world' }
+  let(:tempfile) { Tempfile.new('test') }
 
-    it 'expect_recipe { let_variable } accesses it' do
+  it "converge { file ... } creates the file" do
+    converge {
+      file tempfile.path do
+        content 'test'
+      end
+    }
+    expect(IO.read(tempfile.path)).to eq 'test'
+  end
+
+  context "when there is a let variable" do
+    let(:let_variable) { "test" }
+
+    it "converge { let_variable } accesses it" do
       # Capture the variable outside
       x = nil
-      expect_recipe { x = let_variable }.to be_up_to_date
-      expect(x).to eq 'hello world'
+      converge { x = let_variable }
+      expect(x).to eq 'test'
+    end
+
+    it "converge with a file resource referencing let_variable accesses let_variable" do
+      converge {
+        file tempfile.path do
+          content let_variable
+        end
+      }
+      expect(IO.read(tempfile.path)).to eq 'test'
     end
   end
 end
