a140d3e46c53e97448953b3b332f592d03363b0f
Lamont Granquist <lamont@scriptkiddie.org>
2019-08-29 14:06:01 -0700

    remove cheffish-rspec
    - this appears to not be used in cheffish
    - this appears to not be tested in cheffish
    - it appears all that uses this is chef/chef internally even
      though its parked for some mysterious reason in the cheffish
      gem.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/cheffish/rspec.rb
+++
@@ -1,8 +0,0 @@
-require "cheffish/rspec/chef_run_support"
-require "cheffish/rspec/repository_support"
-require "cheffish/rspec/matchers"
-
-module Cheffish
-  module RSpec
-  end
-end
--- lib/cheffish/rspec/matchers.rb
+++
@@ -1,4 +0,0 @@
-require "cheffish/rspec/matchers/have_updated"
-require "cheffish/rspec/matchers/be_idempotent"
-require "cheffish/rspec/matchers/partially_match"
-require "cheffish/rspec/matchers/emit_no_warnings_or_errors"
